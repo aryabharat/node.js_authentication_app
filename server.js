6be3bc80a90c685bcdc41064be2ace085b9a6044
Bharat Arya <arya.bharat@rocketmail.com>
2018-11-05 01:19:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+    if(req.url === '/login' || req.url === '/' )
+    {
+      fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+    else
+    {
+      fs.readFile('404.html', function(err, daa) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+      });
+    }
+    if (req.method === "POST")
+    {
+      var data = "";
+      req.on("data", function(chunk){
+        data = chunk;
+      });
+      req.on("end", function(chunk){
+        console.log(data);
+      });
+
+    }
+}).listen(8080);
