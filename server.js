0cd696d59c0c3952ad134962136330c2890758b7
Bharat Arya <arya.bharat@rocketmail.com>
2018-11-05 11:59:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,14 +3,48 @@ var fs = require('fs');
 var querystring = require('querystring');
 
 http.createServer(function (req, res) {
-    if(req.url === '/login' || req.url === '/' )
+    if(req.url === '/' )
     {
-      fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
+        fs.readFile('index.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+    }
+    else if(req.url === '/index.html')
+    {
+      if (req.method === "POST")
+      {
+          var d = "";
+          req.on("data", function(chunk)
+          {
+            d += chunk;
+          });
+          req.on("end", function(chunk)
+          {
+            var formdata = querystring.parse(d);
+          //  console.log(formdata.username);
+            if(formdata.username === "bharat" && formdata.pswd === "a@123")
+            {
+              console.log("login successful");
+               fs.readFile('sucess.html', function(err, data) {
+              res.writeHead(200, {'Content-Type': 'text/html'});
+              res.write(data);
+              res.end();
+            });
+            }
+            else {
+               console.log("unsuccessful");
+               fs.readFile('unsucess.html', function(err, data) {
+               res.writeHead(200, {'Content-Type': 'text/html'});
+               res.write(data);
+               res.end();
+            });
+            }
+          });
+      }
+    }
+
     else
     {
       fs.readFile('404.html', function(err, data) {
@@ -19,20 +53,6 @@ http.createServer(function (req, res) {
       res.end();
       });
     }
-    if (req.method === "POST")
-    {
-      var data = "";
-      req.on("data", function(chunk){
-        data += chunk;
-      });
-      req.on("end", function(chunk){
-        var formdata = querystring.parse(data);
-        //console.log(formdata.username);
-        if(formdata.username === "bharat" && formdata.pswd === "a@123")
-        {
-          console.log("sucessfull");
-        }
-      });
 
-    }
-}).listen(8080);
+});
+http.listen(4000);
